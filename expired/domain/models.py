ccb7ac760a0c766bc16ac3c0332c24771af9d83e
kaysam-clone <65505237+kaysam-clone@users.noreply.github.com>
2023-02-27 17:49:18 -0800

    Add files via upload

---
+++ expired/domain/models.py
@@ -0,0 +1,8 @@
+from typing_extensions import Required
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class AllDomain(models.Model):
+    domains = models.JSONField()
